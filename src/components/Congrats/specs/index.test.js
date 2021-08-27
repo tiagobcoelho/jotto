ce1efe5b1a6cec9d442d32f5a8db9422a52ae9d2
@tcoelho1 <tcoelho@advicefront.com>
2021-08-27 11:00:28 +0100

    start tests for GuessesWords

--- src/components/Congrats/specs/index.test.js
+++ src/components/Congrats/specs/index.test.js
@@ -1,9 +1,6 @@
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../../../../test/testUtils';
 import Congrats from '../index';
-
-Enzyme.configure({ adapter: new EnzymeAdapter });
 
 const defaultProps = { success: false }
 /**

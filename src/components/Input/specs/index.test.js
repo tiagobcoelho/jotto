3357b8516b77325d3c35e22c3bcab901c1f3c55d
@tcoelho1 <tcoelho@advicefront.com>
2021-08-27 11:44:43 +0100

    added Input props and prop testing

--- src/components/Input/specs/index.test.js
+++ src/components/Input/specs/index.test.js
@@ -3,14 +3,20 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../../../../test/testUtils';
 import Input from '../index';
 
+const defaultProps = { secretWord: 'party' };
+
 /**
  * Factory function to create a ShallowWrapper for the Input component.
  * @function setup
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />)
-}
+const setup = (secretWord='party') => {
+  return shallow(<Input secretWord={secretWord} />)
+};
+
+test('does not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: 'party' });
+});
 
 test('renders without error', () => {
   const wrapper = setup();

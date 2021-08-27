593f1c02bd0dee73b0fce763d7c66ca5675548a8
@tcoelho1 <tcoelho@advicefront.com>
2021-08-27 14:44:06 +0100

    wired input component state and tests

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState('');
   return (
     <div data-test="component-input">
-      
+      <form className="form-inline">
+        <input 
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text" 
+          placeholder="enter guess" 
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/Input/specs/index.test.js
+++ src/components/Input/specs/index.test.js
@@ -23,3 +23,18 @@ test('renders without error', () => {
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
 });
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: {value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  })
+});

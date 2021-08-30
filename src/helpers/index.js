5cc7711e3311862af016bdf0fedcf3ab517b15f6
@tcoelho1 <tcoelho@advicefront.com>
2021-08-30 14:45:35 +0100

    added helper function for letters matched between guess and secret word

---
+++ src/helpers/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord - Guessed word.
+ * @param {string} secretWord - Secret word.
+ * @returns {number} - Number of letters matched between guessed word and secret word.
+ */
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const secretLetters = secretWord.split('');
+  const guessedLettersSet = new Set(guessedWord);
+  return secretLetters.filter(letter => guessedLettersSet.has(letter)).length;
+};

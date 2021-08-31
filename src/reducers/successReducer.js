b7112e8eb4d5d119f23b945b540b7998fce8e363
@tcoelho1 <tcoelho@advicefront.com>
2021-08-31 16:25:08 +0100

    set up redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function sucessReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new success state.
+ */
+export default (state, action) => {
+  return null;
+};

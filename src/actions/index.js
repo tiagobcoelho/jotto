b000d8ece86fb86feb5e0af0f4026ee1220c4aec
@tcoelho1 <tcoelho@advicefront.com>
2021-08-31 16:34:35 +0100

    added correct guess action creator and tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
 
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+};
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type 'CORRECT_GUESS'
+ */
+export function correctGuess() {
+  return { type: actionTypes.CORRECT_GUESS };
+};
+
 export const getSecretWord = () => {
   // TODO: write actual action in redux / context sections
   return axios.get('http://localhost:3030')
     .then(response => response.data);
 };
+

badb564af06e72cb12c31fd660e25e5d65a8d60a
@tcoelho1 <tcoelho@advicefront.com>
2021-09-01 15:32:10 +0100

    added thunk for guessedWords piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
@@ -14,7 +15,17 @@ export const actionTypes = {
  */
 export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if(guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    };
   };
 };
 

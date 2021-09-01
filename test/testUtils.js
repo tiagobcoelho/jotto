6c847bca116ba3b0b6ce30bd0657a350c9d819f9
@tcoelho1 <tcoelho@advicefront.com>
2021-09-01 14:40:54 +0100

    added redux-thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,6 +1,7 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Create a testing store with imported reducers, middleware and initial state.
@@ -9,7 +10,7 @@ import rootReducer from '../src/reducers';
  * @returns {Store} - Redux store.
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState)
+  return createStore(rootReducer, initialState, applyMiddleware(...middlewares));
 };
 
 /**
